52a2988dcbbd2eedd614ea098fb11461fb0056dc
BOUIBKEROussama <46991351+BOUIBKEROussama@users.noreply.github.com>
2019-01-31 11:00:09 +0100

    Update Moteur.py

--- Moteur.py
+++ Moteur.py
@@ -3,9 +3,6 @@ class Moteur:
         self.v=0
         self.acce=0
         self.distance=0
-    
-    def initDistance(self, distance):
-       self.d=distance
      
     def accelerer(self):
         self.a=+1
