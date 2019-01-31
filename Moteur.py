c40ac1230162456f76cb277fbad1bb99f65a6c24
BOUIBKEROussama <46991351+BOUIBKEROussama@users.noreply.github.com>
2019-01-31 11:01:10 +0100

    Update Moteur.py

--- Moteur.py
+++ Moteur.py
@@ -2,10 +2,7 @@ class Moteur:
     def __init__(self):
         self.v=0
         self.acce=0
-        self.distance=0
      
     def accelerer(self):
         self.a=+1
-    
-    def bougerVers(vecteur):
         
