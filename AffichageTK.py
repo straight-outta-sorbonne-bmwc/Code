d6449bcd5a3baa7d81d582325079be244d5a5874
Utilitair <44241259+Utilitair@users.noreply.github.com>
2019-02-02 17:25:55 +0100

    Add files via upload

--- AffichageTK.py
+++ AffichageTK.py
@@ -1,2 +1,15 @@
 class AffichageTK(Objet):
-	
+	dim = 500										# variable globale qui correspond à la dimension de la fenêtre
+	def __init__(self):
+		self.root = Tk()							# root variable contenant la fenêtre
+		self.root.title('Tuto')						# Nom de la fenêtre	
+		self.can = Canvas(self.root, width=self.dim, height=self.dim, bg='white')			# création de la fenêtre 
+		self.can.pack()																		# permet l'affcichage de la fenêtre																		
+		quitter = Button(self.root, text = 'quitter', command=self.root.quit)				# bouton pour quitter la fenêtre
+		quitter.pack(side=BOTTOM)															# emplacement des boutons
+		self.root.mainloop()
+
+# Programme principal
+if __name__ == '__main__':
+	from tkinter import*							
+	f = Tuto()					# instanciation de l'objet application
