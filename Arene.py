493af37867963b4a4cf7a3664b293ed58db1e2eb
HugoMendes16 <36660724+HugoMendes16@users.noreply.github.com>
2019-02-04 17:35:40 +0100

    Update Arene.py

--- Arene.py
+++ Arene.py
@@ -1,12 +1,20 @@
 import numpy as np
 import random
 class Arene:
-    def __init__(self,taille):
+    def __init__(self,taille,robot):
         self.taille=taille
-        self.Matrice=np.zeros((taille,taille))
+        self.Matrice=np.zeros((self.taille,self.taille))
+        self.robot=robot
     
     def ajouterObstacleAleatoire(self):
         for i in range(15):
             u=random.randint(0,self.taille-1)
             v=random.randint(0,self.taille-1)
             self.Matrice[u][v]=1
+            
+    def avancerXPas(self, Pas):
+        for i in range (Pas):
+            if (self.robot.y+1<self.taille):
+                self.robot.y=self.robot.y+1
+                self.Matrice[self.robot.x][self.robot.y]=2
+                self.Matrice[self.robot.x][self.robot.y-1]=0
