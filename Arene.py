1d6776b2505658f6c340bf3f9b8487e0979b99be
BOUIBKEROussama <46991351+BOUIBKEROussama@users.noreply.github.com>
2019-02-04 16:21:48 +0100

    Update Arene.py

--- Arene.py
+++ Arene.py
@@ -2,10 +2,11 @@ import numpy as np
 import random
 class Arene:
     def __init__(self,taille):
+        self.taille=taille
         self.Matrice=np.zeros((taille,taille))
     
     def ajouterObstacleAleatoire():
         for i in range(15):
-            u=random.randint(0,9)
-            v=random.randint(0,9)
+            u=random.randint(0,self.taille)
+            v=random.randint(0,self.taille)
             self.Matrice[u][v]=1
