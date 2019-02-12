f040f484573594d174acd643837ad405b315dc4f
BOUIBKEROussama <46991351+BOUIBKEROussama@users.noreply.github.com>
2019-02-12 21:38:37 +0100

    Update Arene.py

--- Arene.py
+++ Arene.py
@@ -21,7 +21,44 @@ class Arene:
                 self.robot=robot
                 self.Matrice=np.zeros((self.taille,self.taille))
                 self.Matrice[self.robot.x][self.robot.y]=2
+    
+    
+
+
+def LectureFichier(self,fichier):
+    i=-1
+    if  not (fichier[i]=='t' and fichier[i-1]=='x' and fichier[i-2]=='t'):  #lit un fichier .txt pris en parametre et met toute les valeurs du fichier dans la variable Matrice.
+        print("Erreur dans le type de fichier")
+    else:
+        f = open(fichier,'r')
+        contenu = f.read()
+        x=0
+        y=0
+        for e in contenu:
+            if not(e=='[' or e==']' or e==',' or e==' '):
+                self.Matrice[x][y]=int(e)
+                y+=1
+                if(y==self.taille):
+                    x+=1
+                    y=0
                 
+        f.close()
+        
+        
+        
+def EcritureFichier(self,fichier):                                 #Modifie un fichier .txt pris en parametre et met toute les valeurs de l'arene dedans.
+    i=-1
+    if  not (fichier[i]=='t' and fichier[i-1]=='x' and fichier[i-2]=='t'):
+        print("Erreur dans le type de fichier")
+    else:
+        f = open(fichier,'w')
+        for e in self.Matrice:
+            f.write(str(e))
+        f.close()
+    
+    
+    
+    
     def ajouterObstacleAleatoire(self):
         for i in range(15):
             u=random.randint(0,self.taille-1)
@@ -47,30 +84,36 @@ class Arene:
         	self.Matrice[x][y]=1
             
             
-       def Avancer(self,u,v):
-        if((u>self.taille ) or (v>self.taille) or (u<0) or (v<0)):
+    def Avancer(self,u,v):
+        if((u>self.robot.x ) or (v>self.robot.y) or (self.robot.x<0) or (self.robot.y<0)):
             print("Le point est hors de l'arène")
             return
+        #print(self.Matrice)
+        #print("\n")
         while (self.robot.x != u):
             if (u>self.robot.x):
                 self.robot.x=self.robot.x+1
                 self.Matrice[self.robot.x][self.robot.y]=2
                 self.Matrice[self.robot.x-1][self.robot.y]=0
-                
+                #print(self.Matrice)
+                #print("\n")
             else :
                 self.robot.x=self.robot.x-1
                 self.Matrice[self.robot.x][self.robot.y]=2
                 self.Matrice[self.robot.x+1][self.robot.y]=0
-                
+                #print(self.Matrice)
+                #print("\n")
                 
         while (self.robot.y != v):
             if (v>self.robot.y):
                 self.robot.y=self.robot.y+1
                 self.Matrice[self.robot.x][self.robot.y]=2
                 self.Matrice[self.robot.x][self.robot.y-1]=0
-               
+                #print(self.Matrice)
+                #print("\n")
             else :
                 self.robot.y=self.robot.y-1
                 self.Matrice[self.robot.x][self.robot.y]=2
                 self.Matrice[self.robot.x][self.robot.y+1]=0
-               
+                #print(self.Matrice)
+                #print("\n")
