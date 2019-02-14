25f623f5e204b9bf20138bf5c587eda53229101c
Utilitair <44241259+Utilitair@users.noreply.github.com>
2019-02-14 01:50:57 +0100

    Update Arene.py

--- Code-master/Class/Arene.py
+++ Code-master/Class/Arene.py
@@ -25,36 +25,36 @@ class Arene:
     
 
 
-def LectureFichier(self,fichier):
-    i=-1
-    if  not (fichier[i]=='t' and fichier[i-1]=='x' and fichier[i-2]=='t'):  #lit un fichier .txt pris en parametre et met toute les valeurs du fichier dans la variable Matrice.
-        print("Erreur dans le type de fichier")
-    else:
-        f = open(fichier,'r')
-        contenu = f.read()
-        x=0
-        y=0
-        for e in contenu:
-            if not(e=='[' or e==']' or e==',' or e==' '):
-                self.Matrice[x][y]=int(e)
-                y+=1
-                if(y==self.taille):
-                    x+=1
-                    y=0
-                
-        f.close()
-        
-        
-        
-def EcritureFichier(self,fichier):                                 #Modifie un fichier .txt pris en parametre et met toute les valeurs de l'arene dedans.
-    i=-1
-    if  not (fichier[i]=='t' and fichier[i-1]=='x' and fichier[i-2]=='t'):
-        print("Erreur dans le type de fichier")
-    else:
-        f = open(fichier,'w')
-        for e in self.Matrice:
-            f.write(str(e))
-        f.close()
+    def LectureFichier(self,fichier):
+        i=-1
+        if  not (fichier[i]=='t' and fichier[i-1]=='x' and fichier[i-2]=='t'):  #lit un fichier .txt pris en parametre et met toute les valeurs du fichier dans la variable Matrice.
+            print("Erreur dans le type de fichier")
+        else:
+            f = open(fichier,'r')
+            contenu = f.read()
+            x=0
+            y=0
+            for e in contenu:
+                if not(e=='[' or e==']' or e==',' or e==' ', e=='.'):
+                    self.Matrice[x][y]=int(e)
+                    y+=1
+                    if(y==self.taille):
+                        x+=1
+                        y=0
+                    
+            f.close()
+            
+            
+            
+    def EcritureFichier(self,fichier):                                 #Modifie un fichier .txt pris en parametre et met toute les valeurs de l'arene dedans.
+        i=-1
+        if  not (fichier[i]=='t' and fichier[i-1]=='x' and fichier[i-2]=='t'):
+            print("Erreur dans le type de fichier")
+        else:
+            f = open(fichier,'w')
+            for e in self.Matrice:
+                f.write(str(e))
+            f.close()
     
     
     
@@ -82,14 +82,26 @@ def EcritureFichier(self,fichier):                                 #Modifie un f
             return 
         else:
         	self.Matrice[x][y]=1
-            
+
+    def Collision(self, x, y):
+        """ Si la position (x, y) est le bord de l'arene ou si un 
+        obstacle s'y trouve la fonction renvoie false sinon true"""
+
+        if(x<0 or x>self.taille-1):
+            return false
+        if(y<0 or y>self.taille-1):
+            return false
+        else:
+            if (self.Matrice[x][y]==1):
+                return false
+            return true
             
     def Avancer(self,u,v):
-        if((u>self.robot.x ) or (v>self.robot.y) or (self.robot.x<0) or (self.robot.y<0)):
+        if((u>self.taille ) or (v>self.taille) or (self.taille<0) or (self.taille<0)):
             print("Le point est hors de l'arène")
             return
         #print(self.Matrice)
-        #print("\n")
+        print("\n")
         while (self.robot.x != u):
             if (u>self.robot.x):
                 self.robot.x=self.robot.x+1
@@ -116,4 +128,5 @@ def EcritureFichier(self,fichier):                                 #Modifie un f
                 self.Matrice[self.robot.x][self.robot.y]=2
                 self.Matrice[self.robot.x][self.robot.y+1]=0
                 #print(self.Matrice)
-                #print("\n")
+#print("\n")
+
