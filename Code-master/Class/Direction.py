b607ba1a56c37d007fd477a6fc8980df0e91951c
BOUIBKEROussama <46991351+BOUIBKEROussama@users.noreply.github.com>
2019-02-14 09:14:16 +0100

    Update Direction.py

--- Code-master/Class/Direction.py
+++ Code-master/Class/Direction.py
@@ -6,8 +6,8 @@ class Direction:
         self.v=v
         
     def Rotation(self,angle,robot):#angle positif = sens des aiguille d'une montre et angle negatif = sens inverse
-        xM=self.u-robot.x
-        yM=self.v-robot.y
+        xM=robot.direction.u-robot.x
+        yM=robot.direction.v-robot.y
         self.u= round(xM * math.cos(math.radians(angle)) + yM * math.sin(math.radians(angle)) + robot.x,2)
         self.v = round(- xM * math.sin(math.radians(angle)) + yM * math.cos(math.radians(angle))+ robot.y,2)
         
@@ -15,7 +15,7 @@ class Direction:
 #test=Direction(1.0,0.0)
 #print(test.u)
 #print(test.v)
-#ro=Robot.Robot(0.0,0.0)
-#test.Rotation(-25.3,ro)
+#ro=Robot.Robot(0.0,0.0,50.0,50.0,test)
+#test.Rotation(90,ro)
 #print(test.u)
 #print(test.v)
