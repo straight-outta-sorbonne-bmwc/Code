85df85562a2c0083e1b982f575114d54e7c05fbf
BOUIBKEROussama <46991351+BOUIBKEROussama@users.noreply.github.com>
2019-02-14 08:52:49 +0100

    Update Robot.py

--- Code-master/Class/Robot.py
+++ Code-master/Class/Robot.py
@@ -1,5 +1,8 @@
 class Robot:
-    taille = 20
-    def __init__(self,x,y):
+    def __init__(self,x,y,longueur,largeur,direction):
         self.x=x
         self.y=y
+        self.longueur=longueur
+        self.largeur=largeur
+        self.direction=direction
+        Coordonne=[(self.x,self.y),(self.x+self.longueur,self.y),(self.x+longueur,self.y+largeur),(self.x,self.y+self.largeur)]
