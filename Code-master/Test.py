afa6920b2163dae2f7f7330ee8d6771f33df1891
COLLAS <julien.collas@yahoo.com>
2019-02-21 09:38:07 +0100

    Supression de deux fichiers inutiles

--- Code-master/Test.py
+++
@@ -1,9 +0,0 @@
-import Arene
-import Robot
-
-
-robot = Robot.Robot(5.0,5.0,10,10)
-carre = Arene.Arene(10, robot)
-carre.ajouterObstacleAleatoire()
-carre.EcritureFichier("fichier.txt")
-#carre.LectureFichier("fichier.txt")
