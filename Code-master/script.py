0402455bedc5cfd1edea952101155a85ca80724b
franfran98 <46990864+franfran98@users.noreply.github.com>
2019-04-03 20:36:12 +0200

    Update

--- Code-master/script.py
+++ Code-master/script.py
@@ -1,17 +1,33 @@
-from simulation import *
+from simulation import robot, affichage, arene
+from simulation import strategie
 from threading import Thread
 import time
+
+#import Robot2I013
 
 robot=robot.Robot(500, 500, 50, 50)
 arene=arene.Arene(robot)
 fenetre=affichage.AffichageTK(arene, robot)
-paralele=Thread(target=fenetre.start_affichage)
-paralele.start()
-while not arene.stop:
+paralelle=Thread(target=fenetre.start_affichage)
+paralelle.start()
+strat=strategie.strategie_avance(100, robot, 50)
+thread_strat=Thread(target=strat.update)
+thread_strat.start()
+strat.start()
+while not strat.stop():
 	arene.update()
-	time.sleep(0.2)
-	print("test")
+	strat.update()
+	time.sleep(0.1)
+	print(robot.x, robot.y)
 time.sleep(1)
+
+
+# robot=Robot2I013()
+# strat=strategie.strategie_avance(10, robot, 10)
+# strat.start()
+# while not strat.stop() :
+# 	strat.update()
+# 	time.sleep(0.2)
 
 
 
