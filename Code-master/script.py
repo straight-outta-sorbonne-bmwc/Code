a9cce8066352c9df94276781ef2fa9b7c36100cd
COLLAS <julien.collas@yahoo.com>
2019-04-04 12:19:37 +0200

    sscript mode simulation

--- Code-master/script.py
+++ Code-master/script.py
@@ -1,22 +1,22 @@
 from threading import Thread
 import time
 
-#from simulation import robot, affichage, arene
+from simulation import robot, affichage, arene
 from simulation import strategie
-from robot2I013 import Robot2I013
+#from robot2I013 import Robot2I013
 
-robot=Robot2I013()
-#robot=robot.Robot(500, 500, 50, 50)
-#arene=arene.Arene(robot)
-#fenetre=affichage.AffichageTK(arene, robot)
-#paralelle=Thread(target=fenetre.start_affichage)
-#paralelle.start()
+#robot=Robot2I013()
+robot=robot.Robot(500, 500, 50, 50)
+arene=arene.Arene(robot)
+fenetre=affichage.AffichageTK(arene, robot)
+paralelle=Thread(target=fenetre.start_affichage)
+paralelle.start()
 strat=strategie.strategie_tourner_droite(360, robot, 500)
 thread_strat=Thread(target=strat.update)
 thread_strat.start()
 strat.start()
 while not strat.stop():
-	#arene.update()
+	arene.update()
 	strat.update()
 	time.sleep(0.001)
 time.sleep(1)
