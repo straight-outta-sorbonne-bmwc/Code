ff70af581cf521c95c9a77559964f04ecfcd4f0a
franfran98 <46990864+franfran98@users.noreply.github.com>
2019-04-11 12:20:44 +0200

    update du script

--- Code-master/script.py
+++ Code-master/script.py
@@ -1,25 +1,27 @@
+from simulation import robot, affichage, arene
+from simulation import strategie
 from threading import Thread
 import time
 
-from simulation import robot, affichage, arene
-from simulation import strategie
-#from robot2I013 import Robot2I013
+#import Robot2I013
 
-#robot=Robot2I013()
 robot=robot.Robot(500, 500, 50, 50)
 arene=arene.Arene(robot)
 fenetre=affichage.AffichageTK(arene, robot)
 paralelle=Thread(target=fenetre.start_affichage)
 paralelle.start()
-strat=strategie.strategie_tourner_droite(360, robot, 500)
+strat=strategie.strategie_carre(robot, 100)
 thread_strat=Thread(target=strat.update)
 thread_strat.start()
 strat.start()
+
 while not strat.stop():
 	arene.update()
 	strat.update()
-	time.sleep(0.001)
+	time.sleep(0.01)
+	#print(robot.x, robot.y)
 time.sleep(1)
+
 
 
  	
