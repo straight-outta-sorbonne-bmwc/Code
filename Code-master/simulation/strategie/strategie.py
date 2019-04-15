bde0d55f30b062581382e39faf3f595903665061
BOUIBKEROussama <46991351+BOUIBKEROussama@users.noreply.github.com>
2019-04-15 17:48:14 +0200

    Update strategie.py

--- Code-master/simulation/strategie/strategie.py
+++ Code-master/simulation/strategie/strategie.py
@@ -39,7 +39,6 @@ class strategie_avance:
 			self.robot.set_motor_dps(self.robot.MOTOR_LEFT+self.robot.MOTOR_RIGHT,0)
 		return res
 
-
 class strategie_tourner_gauche:
 	def __init__(self, angle, robot, vitesse):
 		self.angle=angle
@@ -124,3 +123,87 @@ class strategie_carre:
 	def stop(self):
 		return self.nb_tour>=7
 		
+
+class strategie_triangle_equi:
+	def __init__(self,robot,vitesse):
+		self.robot=robot
+		self.vitesse=vitesse
+		self.liste=[strategie_avance(200,self.robot,self.vitesse),strategie_tourner_droite(120,self.robot,self.vitesse)]
+		self.nb_tour=0
+		
+	def start(self):
+		self.liste[self.nb_tour%2].start()
+
+	def update(self):
+		if self.stop():
+			return
+		
+		if self.liste[self.nb_tour%2].stop():
+			self.nb_tour+=1
+			self.liste[self.nb_tour%2].start()
+		
+		else:
+			self.liste[self.nb_tour%2].update()
+		print(self.robot.x,self.robot.y)
+		
+	def stop(self):
+		return self.nb_tour>=10
+
+
+class strategie_polygone:
+	def __init__(self,robot,vitesse,n):
+		self.robot=robot
+		self.vitesse=vitesse
+		self.n=n
+		self.liste=[strategie_avance(30/self.n,self.robot,self.vitesse),strategie_tourner_droite(360/n,self.robot,self.vitesse)]
+		print((self.n-2)*math.pi/self.n)
+		self.nb_tour=0
+		
+	def start(self):
+		self.liste[self.nb_tour%2].start()
+
+	def update(self):
+		if self.stop():
+			return
+		
+		if self.liste[self.nb_tour%2].stop():
+			self.nb_tour+=1
+			self.liste[self.nb_tour%2].start()
+		
+		else:
+			self.liste[self.nb_tour%2].update()
+		print(self.robot.x,self.robot.y)
+
+	def stop(self):
+		return self.nb_tour>=self.n*2
+
+
+class strategie_mur:
+	def __init__(self,robot,vitesse):
+		self.robot=robot
+		self.vitesse=vitesse
+		self.liste=[strategie_avance(1,self.robot,self.vitesse),strategie_tourner_droite(90,self.robot,self.vitesse)]
+		self.nb_tour=0
+
+	def start(self):
+		self.liste[0].start()
+		
+
+	def update(self):
+		if self.stop():
+			return
+		
+		if self.liste[self.nb_tour%2].stop() :#and (self.robot.x>=800 or self.robot.x<=0 or self.robot.y>=800 or self.robot.y<=0):
+			if (self.robot.x>=910 or self.robot.x<=0 or self.robot.y>=910 or self.robot.y<=0):
+				self.nb_tour+=1
+				self.liste[self.nb_tour%2].start()
+			else:
+				if self.nb_tour%2 == 0:
+					self.liste[self.nb_tour].start()
+	
+		else:
+			self.liste[self.nb_tour%2].update()
+		print(self.robot.x,self.robot.y)
+
+	def stop(self):
+		return self.nb_tour>=5
