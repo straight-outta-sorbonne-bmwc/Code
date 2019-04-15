8229c679b1f4c18400da77c7f4579047102f0ad2
BOUIBKEROussama <46991351+BOUIBKEROussama@users.noreply.github.com>
2019-04-15 17:49:52 +0200

    Update and rename script.py to tmesolo.py

--- Code-master/tmesolo.py
+++ Code-master/tmesolo.py
@@ -10,7 +10,17 @@ arene=arene.Arene(robot)
 fenetre=affichage.AffichageTK(arene, robot)
 paralelle=Thread(target=fenetre.start_affichage)
 paralelle.start()
-strat=strategie.strategie_carre(robot, 100)
+#
+#IMPORTANT : Toutes les strategies,fonctions etc sont listés dans le fichier tmesolo.txt et sont aussi disponibles sur affichage.py et strategie.py
+#
+#Exercice 1 Question 1: Voir fichier arene
+#Exercice 1 Question 2: Voir fichier arene
+#Exercice 2 Question 1:
+strat=strategie.strategie_triangle_equi(robot, 200)
+#Exercice 2 Question 2:
+#strat=strategie.strategie_polygone(robot, 200,20)
+#Exercice 2 Question 3:
+#strat=strategie.strategie-mur(robot, 200)
 thread_strat=Thread(target=strat.update)
 thread_strat.start()
 strat.start()
