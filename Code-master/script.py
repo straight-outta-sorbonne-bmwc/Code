a374410f86b5800511631d570ac0b9b08b306cd9
COLLAS <julien.collas@yahoo.com>
2019-05-26 10:59:55 +0200

    Modifications pendant la réu

--- Code-master/script.py
+++ Code-master/script.py
@@ -1,25 +1,24 @@
-from simulation import robot, affichage, arene
+#from simulation import robot, affichage, arene
 from simulation import strategie
 from threading import Thread
 import time
 import math
 
-#import Robot2I013
+import robot2I013
 
-robot=robot.Robot(500, 500, 50, 50)
-arene=arene.Arene(robot)
+robot=robot2I013.Robot2I013()
+#arene=arene.Arene(robot)
 
-arene.ajouter_obstacle_aleatoire()
-fenetre=affichage.AffichageTK(arene, robot)
-paralelle=Thread(target=fenetre.start_affichage)
-paralelle.start()
-strat=strategie.strategie_avance(2000,robot, -200)
+#arene.ajouter_obstacle_aleatoire()
+#fenetre=affichage.AffichageTK(arene, robot)
+#paralelle=Thread(target=fenetre.start_affichage)
+#paralelle.start()
+strat=strategie.strategie_carre(robot, 800)
 thread_strat=Thread(target=strat.update)
 thread_strat.start()
 strat.start()
-
 while not strat.stop():
-	arene.update()
+	#arene.update()
 	strat.update()
 	time.sleep(0.01)
 	
