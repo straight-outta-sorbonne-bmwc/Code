a92f6f664a9313c32595a2d7fd4a35c207bb1781
franfran98 <46990864+franfran98@users.noreply.github.com>
2019-05-26 12:55:11 +0200

    Update script.py

--- Code-master/script.py
+++ Code-master/script.py
@@ -1,24 +1,25 @@
-#from simulation import robot, affichage, arene
+from simulation import robot, affichage, arene
 from simulation import strategie
 from threading import Thread
 import time
 import math
 
-import robot2I013
+#import Robot2I013
 
-robot=robot2I013.Robot2I013()
-#arene=arene.Arene(robot)
+robot=robot.Robot(500, 500, 50, 50)
+arene=arene.Arene(robot)
+arene.ajouter_obstacle_aleatoire()
 
-#arene.ajouter_obstacle_aleatoire()
-#fenetre=affichage.AffichageTK(arene, robot)
-#paralelle=Thread(target=fenetre.start_affichage)
-#paralelle.start()
-strat=strategie.strategie_carre(robot, 800)
+fenetre=affichage.AffichageTK(arene)
+paralelle=Thread(target=fenetre.start_affichage)
+paralelle.start()
+strat=strategie.strategie_carre(robot, 200)
 thread_strat=Thread(target=strat.update)
 thread_strat.start()
 strat.start()
+
 while not strat.stop():
-	#arene.update()
+	arene.update()
 	strat.update()
 	time.sleep(0.01)
 	
